1b7f119e57367012fdad28f0aa311edb0d191a28
佑亮 合屋 <ryuguren@outlook.jp>
2018-12-07 23:41:07 +0900

    ちょっとした変更

--- Hero Team/Assets/Script/Enemies/Boss.cs
+++ Hero Team/Assets/Script/Enemies/Boss.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossMove : MonoBehaviour {
+public class Boss : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
