6af2faff2a4c5267bda2f57c962ef9764a99e566
佑亮 合屋 <ryuguren@outlook.jp>
2018-12-08 13:28:58 +0900

    変更点忘れたけどとりあえず

--- Hero Team/Assets/Script/WallHitter.cs
+++ Hero Team/Assets/Script/WallHitter.cs
@@ -1,23 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WallHitter : MonoBehaviour
 {
-
+    [SerializeField]
+    private Camera mainCamera;    //映っているか半手数rカメラ参照
+    [SerializeField]
+    private GameObject wallPrefab;
+    private GameObject [] wallObjects;
     // Use this for initialization
-    void Start()
+    private void Start()
     {
-
+        WallSetting();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
 
     }
 
-    bool IsHit(GameObject myObject)
+    private void WallSetting()
+    {
+        wallObjects = new GameObject[4];
+        wallObjects[0] = Instantiate(wallPrefab, new Vector2(GetScreenTopLeft().x, GetScreenTopLeft().y), Quaternion.identity);
+        wallObjects[1] = Instantiate(wallPrefab, new Vector2(GetScreenTopLeft().x, GetScreenBottomRight().y), Quaternion.identity);
+        wallObjects[2] = Instantiate(wallPrefab, new Vector2(GetScreenBottomRight().x, GetScreenTopLeft().y), Quaternion.identity);
+        wallObjects[3] = Instantiate(wallPrefab, new Vector2(GetScreenBottomRight().x, GetScreenBottomRight().y), Quaternion.identity);
+    }
+
+    private Vector3 GetScreenTopLeft()
+    {
+        // 画面の左上を取得
+        Vector3 topLeft = mainCamera.ScreenToWorldPoint(Vector3.zero);
+        // 上下反転させる
+        topLeft.Scale(new Vector3(1f, -1f, 1f));
+        return topLeft;
+    }
+
+    private Vector3 GetScreenBottomRight()
+    {
+        // 画面の右下を取得
+        Vector3 bottomRight = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, 0.0f));
+        // 上下反転させる
+        bottomRight.Scale(new Vector3(1f, -1f, 1f));
+        return bottomRight;
+    }
+
+    public bool IsHit(GameObject myObject)
     {
         return false;
     }
