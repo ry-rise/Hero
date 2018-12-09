704ea4b4ab383dcaee66a4e6add87ae8873bac8f
ry-rise <ry-rise@example.com>
2018-12-09 21:02:30 +0900

    敵を徐々に落とす

--- Hero Team/Assets/Script/Enemies/EnemyMove.cs
+++ Hero Team/Assets/Script/Enemies/EnemyMove.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 
 public class EnemyMove : MonoBehaviour {
     public List<EnemyAttack> Enemies;
-	void Start (){
-		
-	}
-	
-	void Update () {
-		
-	}
+
+    void FixedUpdate()
+    {
+        for (int i = 0; i <= Enemies.Count - 1; i += 1)
+        {
+            Enemies[i].gameObject.transform.Translate(0, -0.01f, 0);
+        }
+    }
 }
