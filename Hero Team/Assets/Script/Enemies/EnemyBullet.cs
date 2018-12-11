b6c455b8e61539778b7a6b89343f6c70592e5ea6
佑亮 合屋 <ryuguren@outlook.jp>
2018-12-11 18:31:39 +0900

    敵の攻撃を受けたらBarが縮むよ

--- Hero Team/Assets/Script/Enemies/EnemyBullet.cs
+++ Hero Team/Assets/Script/Enemies/EnemyBullet.cs
@@ -2,38 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBullet : MonoBehaviour {
+public class EnemyBullet : MonoBehaviour
+{
     [SerializeField]
     private float Speed;
     private float X;
     private float Y;
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         X = this.transform.position.x;
         Y = this.transform.position.y;
-        this.transform.position = new Vector3(X, Y - Speed * Time.deltaTime ,0);
+        this.transform.position = new Vector3(X, Y - Speed * Time.deltaTime, 0);
 
-        if(transform.position.y < -7)
+        if (transform.position.y < -7)
         {
             Destroy(this.gameObject);
         }
 
-	}
+    }
 
     void OnTriggerEnter2D(Collider2D Target)
     {
-
-    
-        if (Target.gameObject.tag == "Bar" )
+        if (Target.gameObject.tag == "Bar")
         {
-           
+            Target.GetComponent<Bar>().Damage(1);
             Destroy(this.gameObject);
-
         }
-     
     }
 }
