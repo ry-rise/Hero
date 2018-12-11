39c4ab5f36f9acf68fc0e7fd958d3173763301e4
kokuhei8001 <kokuhei8001@gmail.com>
2018-12-11 19:51:30 +0900

    「敵の残数０でクリア表示」を実装

--- Hero Team/Assets/Script/GameManager.cs
+++ Hero Team/Assets/Script/GameManager.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+//コクブ
+public class GameManager : MonoBehaviour
+{
+    private int EnemyCount = 0; //敵残数
+    private int PlayerLife = 3; //プレイヤー残機
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private EnemyMove Enemy;
+
+
+    private void Start()
+    {
+        Enemy = GameObject.Find("EnemyManager").GetComponent<EnemyMove>();
+    }
+
+    private void Update()
+    {
+        EnemyCount = Enemy.GetEnemiesCount();
+        //敵残数が０になったらクリア
+        if (EnemyCount <= 0)
+        {
+            GameClear();
+        }
+    }
+
+    //ゲームクリア時の挙動
+    void GameClear()
+    {
+        Debug.Log("Game CLEAR!!");
+    }
+
+    //ゲームオーバー時の挙動
+    void GameOver()
+    {
+        Debug.Log("Game Over!!");
+    }
 }
