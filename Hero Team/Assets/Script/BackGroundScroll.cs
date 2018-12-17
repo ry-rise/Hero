e8bbbca820892d2733ffe7e5ae09443503886517
DESKTOP-R9R4S4N\vantan <ryo@yahoo.co.jp>
2018-12-17 23:27:22 +0900

    スクロール用スクリプト追加

---
+++ Hero Team/Assets/Script/BackGroundScroll.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundScroll : MonoBehaviour {
+
+
+	// Use this for initialization
+    public void Scroll()
+    {
+
+            float scroll = Mathf.Repeat(Time.time * 0.2f, 1);
+            Vector2 offset = new Vector2(0, scroll);
+            GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+}
