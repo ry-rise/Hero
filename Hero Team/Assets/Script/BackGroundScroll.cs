e7dbb64fba815ea32aa66b44df3ede51b4975657
DESKTOP-R9R4S4N\vantan <ryo@yahoo.co.jp>
2018-12-17 23:44:30 +0900

    BackGroundScroll編集

--- Hero Team/Assets/Script/BackGroundScroll.cs
+++ Hero Team/Assets/Script/BackGroundScroll.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BackGroundScroll : MonoBehaviour {
-
-
+    public bool ScrollFlag = false;
+    private float timer = 0.0f;
+    [SerializeField]
+    float EndTime;
 	// Use this for initialization
     public void Scroll()
     {
@@ -20,6 +22,17 @@ public class BackGroundScroll : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        if(ScrollFlag == true)
+        {
+            timer += Time.deltaTime;
+            Scroll();
+            if(timer > EndTime)
+            {
+                timer = 0.0f;
+                ScrollFlag = false;
+               
+            }
+        }
 
     }
 }
