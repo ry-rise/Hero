76f5fb2f889236dbcac2bdb261d82f6612420789
佐藤 凌 <ryo@yahoo.co.jp>
2019-01-31 13:17:32 +0900

    タイトルシーンからメインゲームへの移行 リザルトシーンでの得点取得とタイトルシーンへの移行

---
+++ Hero Team/Assets/Script/UI/CallResult.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CallResult : MonoBehaviour {
+    [SerializeField]
+    private Text YPText;
+    [SerializeField]
+    private Text EnemyText;
+    [SerializeField]
+    private Text RefectionText;
+    [SerializeField]
+    private Text ItemText;
+    [SerializeField]
+    private Text TotalText;
+	// Use this for initialization
+	void Start () {
+        EnemyText.text = "倒した敵   " + GameManager.EnemyScore;
+        RefectionText.text = "ぶっとばし   " + GameManager.BarScore;
+        ItemText.text = "アイテム   " + GameManager.ItemScore;
+        TotalText.text = "合計スコア   " + GameManager.TotalScore;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
