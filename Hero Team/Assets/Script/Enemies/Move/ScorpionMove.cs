486684c05a7440fef366375f3f957c36eaff7283
佑亮 合屋 <ryuguren@outlook.jp>
2019-02-08 19:46:23 +0900

    サソリ若干変更

--- Hero Team/Assets/Script/Enemies/Move/ScorpionMove.cs
+++ Hero Team/Assets/Script/Enemies/Move/ScorpionMove.cs
@@ -5,20 +5,20 @@ public class ScorpionMove : BaseEnemyMove {
 
     [SerializeField] private Animator scopionAnimation;
     [SerializeField] private GameObject cactus;
-    private float time;
-    [SerializeField]
-    private Scorpion scorpion;
+    private float timeCount;
+    [SerializeField] private float time;
+    [SerializeField] private Scorpion scorpion;
 
     public void Update()
     {
-        time += Time.deltaTime;
-        if (time >= 3.0f)
+        timeCount += Time.deltaTime;
+        if (timeCount >= time)
         {
             GameObject CactusInstance = Instantiate(cactus,
                                         new Vector3(Random.Range(-2.0f, 2.0f), Random.Range(0.0f, -2.0f), 0),
                                         Quaternion.identity);
             scorpion.CactusObjects.Add(CactusInstance.GetComponent<ObjectEnemy>());
-            time = 0;
+            timeCount = 0;
         }
     }
 
