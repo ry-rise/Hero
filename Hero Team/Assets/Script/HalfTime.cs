0a9b2aa26a86da3e992525511e6a7b8bda16f64c
米倉 凌 <ry-rise@example.com>
2019-02-12 14:36:23 +0900

    ハーフタイム修正中 その２

--- Hero Team/Assets/Script/HalfTime.cs
+++ Hero Team/Assets/Script/HalfTime.cs
@@ -5,13 +5,58 @@ using UnityEngine.SceneManagement;
 public class HalfTime : MonoBehaviour {
     private static int life;
     private Text HeroPointText;
+    private string TelopInit = "次の戦いに備えましょう";
+    private string TelopRecovery = "HPは回復したわ。これでまた思いっきりぶっとばせるわね。";
+    private string TelopNext = "さあ、先に進むわよ！";
+    private GameObject[] hearts;
+    [SerializeField] private Sprite heartDark;
+    [SerializeField] private Sprite heartLight;
+
     private void Awake()
     {
+        hearts = new GameObject[3];
+        hearts[0] = GameObject.Find("Heart1");
+        hearts[1] = GameObject.Find("Heart2");
+        hearts[2] = GameObject.Find("Heart3");
+        GameManager.PlayerLife = 1;
+        switch (GameManager.PlayerLife)
+        {
+            case 1:
+                hearts[0].GetComponent<Image>().sprite = heartLight;
+                hearts[1].GetComponent<Image>().sprite = heartDark;
+                hearts[2].GetComponent<Image>().sprite = heartDark;
+                break;
+            case 2:
+                hearts[0].GetComponent<Image>().sprite = heartLight;
+                hearts[1].GetComponent<Image>().sprite = heartLight;
+                hearts[2].GetComponent<Image>().sprite = heartDark;
+                break;
+            case 3:
+                hearts[0].GetComponent<Image>().sprite = heartLight;
+                hearts[1].GetComponent<Image>().sprite = heartLight;
+                hearts[2].GetComponent<Image>().sprite = heartLight;
+                break;
+        }
         HeroPointText = GameObject.Find("HeroPointText").GetComponent<Text>();
         HeroPointText.text = GameManager.HeroPoint.ToString();
     }
+    private void Update()
+    {
+        if (GameManager.HeroPoint <= 0)
+        {
+            GameObject.Find("ButtonRecovery").GetComponent<Button>().interactable = false;
+        }
+    }
     public void OnClick(GameObject buttonObject)
     {
+        //「HP回復」ボタン
+        if (buttonObject.name == "ButtonRecovery")
+        {
+            GameManager.HeroPoint -= 1;
+            life += 1;
+            GameObject.Find("ButtonRestore").GetComponent<Button>().interactable = true;
+
+        }
         //「元に戻す」ボタン
         if (buttonObject.name == "ButtonRestore")
         {
@@ -29,7 +74,7 @@ public class HalfTime : MonoBehaviour {
         {
             GameManager.PlayerLife += life;
             GameManager.HeroPoint -=  life;
-            SceneManager.LoadScene("MainGame 1");
+            SceneManager.LoadScene("");
         }
         //「いいえ」を押すと前の画面に戻る
         if (buttonObject.name == "ButtonNo")
