099e305a46a641793ff709dde453fd732b0ebe57
米倉 凌 <ry-rise@example.com>
2019-02-13 16:58:08 +0900

    ハーフタイムほぼ完成？

--- Hero Team/Assets/Script/HalfTime.cs
+++ Hero Team/Assets/Script/HalfTime.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 public class HalfTime : MonoBehaviour {
     private static int life;
     private Text HeroPointText;
+    private Text TelopText;
     private string TelopInit = "次の戦いに備えましょう";
-    private string TelopRecovery = "HPは回復したわ。これでまた思いっきりぶっとばせるわね。";
+    private string TelopRecovery = "HPは回復したわ。\nこれでまた思いっきりぶっとばせるわね。";
     private string TelopNext = "さあ、先に進むわよ！";
     private GameObject[] hearts;
+    private GameObject buttonRecovery;
+    private GameObject goddessImage;
     [SerializeField] private Sprite heartDark;
     [SerializeField] private Sprite heartLight;
+    [SerializeField] private Sprite recoveryOn;
+    [SerializeField] private Sprite recoveryOff;
 
     private void Awake()
     {
@@ -18,7 +23,26 @@ public class HalfTime : MonoBehaviour {
         hearts[0] = GameObject.Find("Heart1");
         hearts[1] = GameObject.Find("Heart2");
         hearts[2] = GameObject.Find("Heart3");
+        buttonRecovery = GameObject.Find("ButtonRecovery");
+        goddessImage = GameObject.Find("GoddessImage");
         GameManager.PlayerLife = 1;
+        GameManager.HeroPoint = 10;
+        HeroPointText = GameObject.Find("HeroPointText").GetComponent<Text>();
+        HeroPointText.text = GameManager.HeroPoint.ToString();
+        TelopText = GameObject.Find("TelopText").GetComponent<Text>();
+        TelopText.text = TelopInit;
+        HeartSpriteChange();
+    }
+    private void Update()
+    {
+        if (GameManager.HeroPoint <= 0)
+        {
+            buttonRecovery.GetComponent<Image>().sprite = recoveryOff;
+            buttonRecovery.GetComponent<Button>().interactable = false;
+        }
+    }
+    private void HeartSpriteChange()
+    {
         switch (GameManager.PlayerLife)
         {
             case 1:
@@ -35,16 +59,9 @@ public class HalfTime : MonoBehaviour {
                 hearts[0].GetComponent<Image>().sprite = heartLight;
                 hearts[1].GetComponent<Image>().sprite = heartLight;
                 hearts[2].GetComponent<Image>().sprite = heartLight;
+                buttonRecovery.GetComponent<Image>().sprite = recoveryOff;
+                buttonRecovery.GetComponent<Button>().interactable = false;
                 break;
-        }
-        HeroPointText = GameObject.Find("HeroPointText").GetComponent<Text>();
-        HeroPointText.text = GameManager.HeroPoint.ToString();
-    }
-    private void Update()
-    {
-        if (GameManager.HeroPoint <= 0)
-        {
-            GameObject.Find("ButtonRecovery").GetComponent<Button>().interactable = false;
         }
     }
     public void OnClick(GameObject buttonObject)
@@ -53,33 +70,46 @@ public class HalfTime : MonoBehaviour {
         if (buttonObject.name == "ButtonRecovery")
         {
             GameManager.HeroPoint -= 1;
+            GameManager.PlayerLife += 1;
+            HeroPointText.text = GameManager.HeroPoint.ToString();
             life += 1;
+            HeartSpriteChange();
             GameObject.Find("ButtonRestore").GetComponent<Button>().interactable = true;
-
+            TelopText.text = TelopRecovery;
         }
         //「元に戻す」ボタン
         if (buttonObject.name == "ButtonRestore")
         {
             GameManager.HeroPoint += life;
+            GameManager.PlayerLife -= life;
+            HeroPointText.text = GameManager.HeroPoint.ToString();
             life = 0;
+            HeartSpriteChange();
+            buttonRecovery.GetComponent<Image>().sprite = recoveryOn;
+            buttonRecovery.GetComponent<Button>().interactable = true;
             buttonObject.GetComponent<Button>().interactable = false;
+            TelopText.text = TelopInit;
         }
         //「つぎへ」を押すと確認画面に
         if (buttonObject.name == "ButtonNext")
         {
-            transform.Find("CheckPanel").gameObject.SetActive(true);
+            TelopText.text = TelopNext;
+            Transform checkPanel = transform.Find("CheckPanel");
+            checkPanel.gameObject.SetActive(true);
+            goddessImage.transform.SetParent(checkPanel);
         }
         //「はい」を押すとステージに移動
         if (buttonObject.name == "ButtonYes")
         {
-            GameManager.PlayerLife += life;
-            GameManager.HeroPoint -=  life;
             SceneManager.LoadScene("");
         }
         //「いいえ」を押すと前の画面に戻る
         if (buttonObject.name == "ButtonNo")
         {
-            transform.Find("CheckPanel").gameObject.SetActive(false);
+            Transform checkPanel = transform.Find("CheckPanel");
+            checkPanel.gameObject.SetActive(false);
+            goddessImage.transform.SetParent(transform);
+            TelopText.text = TelopInit;
         }
     }
 }
