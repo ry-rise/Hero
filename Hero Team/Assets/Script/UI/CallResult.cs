a9b2fea5c6d4fc76c538233cd5c2c9317e0ad2b5
佑亮 合屋 <ryuguren@outlook.jp>
2019-02-15 08:42:20 +0900

    忘れ物

--- Hero Team/Assets/Script/UI/CallResult.cs
+++ Hero Team/Assets/Script/UI/CallResult.cs
@@ -18,23 +18,23 @@ public class CallResult : MonoBehaviour {
     private Text GoddessText;
     public int[] StandardScore;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         HeroPointText.text = "勇者ポイント" + GameManager.HeroPointScore;
         EnemyText.text = "倒した敵   " + GameManager.EnemyScore;
         RefectionText.text = "ぶっとばし   " + GameManager.BarScore;
         ItemText.text = "アイテム   " + GameManager.ItemScore;
         TotalText.text = "合計スコア   " + GameManager.TotalScore;
-       if (GameManager.TotalScore >= StandardScore[2])
+        if (GameManager.TotalScore >= StandardScore[2] * (1 << GameManager.SelectLevel))
         {
             GoddessText.text = "･･･凄まじい活躍ね。もはや神の領域だわ。";
         }
-       else if (GameManager.TotalScore >= StandardScore[1])
+        else if (GameManager.TotalScore >= StandardScore[1] * (1 << GameManager.SelectLevel))
         {
             GoddessText.text = "まさに勇者に相応しい活躍ね。お見事！";
         }
-        else if (GameManager.TotalScore >= StandardScore[0])
+        else if (GameManager.TotalScore >= StandardScore[0] * (1 << GameManager.SelectLevel))
         {
-           
             GoddessText.text = "ひとまず世界は救われたわね。おつかれさま。";
         }
     }
